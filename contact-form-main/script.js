bc7f8f36dd59a7b4a07d1953ab0891a8217912b9
Isra <Israchokwatana@gmail.com>
2025-02-16 21:28:42 +0700

    alert to JS

---
+++ contact-form-main/script.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function(event) { // Event listener for form submission
+  event.preventDefault(); // Prevent default form submission
+  alert("Thanks for submitting the form!");
+});
