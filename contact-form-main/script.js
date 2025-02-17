532135f40caa820e26223cde913eba0583b85812
Isra <Israchokwatana@gmail.com>
2025-02-17 21:24:41 +0700

    swal added

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -1,4 +1,42 @@
-document.querySelector("form").addEventListener("submit", function(event) { // Event listener for form submission
-  event.preventDefault(); // Prevent default form submission
-  alert("Thanks for submitting the form!");
+const form = document.querySelector('form'); // Get form element
+
+form.addEventListener("submit", function(event) { // Event listener for form submission
+  event.preventDefault(); // Prevent default form submission before JS logic runs
+
+  //Custom validation
+  const email = document.getElementById("email").value;
+  const msg = document.getElementById("Message").value;
+
+  // Email validation
+  if (!emailIsValid(email)) {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Your email is invalid!",
+    });
+    return;
+  }
+
+  if (msg.length < 10) {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Your message must be at least 10 characters long!",
+    });
+    return;
+  }
+
+  // Submit the form after message pop up
+  Swal.fire({ 
+    title: "Success!",
+    text: "Your message has been sent!",
+    icon: "success",
+  }).then(() => {
+    form.submit();
+  });
 });
+
+// Regular expression for email validation
+function emailIsValid(input) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
+}
